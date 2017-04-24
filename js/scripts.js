549dfa31beec952595db2d49fff291162d32f1e0
derekarani <derekarani@gmail.com>
2017-04-24 17:52:22 +0300

    Business logic of the interface such as addition,subtraction,division,multiplication,remainder complete

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+var add = function() {
+	var number1 = parseInt(prompt("Write the first number"));
+	var number2 = parseInt(prompt("Write the second number"));
+	result=number1+ number2;
+	alert("The answer is "+result+".");
+};
+
+var subtract = function(){
+  var number1 = parseInt(prompt("Write the first number"));
+	var number2 = parseInt(prompt("Write the second number"));
+	result=number1- number2;
+	alert("The answer is "+result+".");
+
+}
+
+var division = function(){
+  var number1 = parseInt(prompt("Write the first number"));
+	var number2 = parseInt(prompt("Write the second number"));
+	result=number1/ number2;
+	alert("The answer is "+result+".");
+
+}
+
+var multiplication = function(){
+  var number1 = parseInt(prompt("Write the first number"));
+	var number2 = parseInt(prompt("Write the second number"));
+	result=number1* number2;
+	alert("The answer is "+result+".");
+
+}
+
+var remainder = function(){
+  var number1 = parseInt(prompt("Write the first number"));
+	var number2 = parseInt(prompt("Write the second number"));
+	result=number1% number2;
+	alert("The answer is "+result+".");
+
+}
